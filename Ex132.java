898b20b88c5890ba8634a412f459b819568bf813
Roy Assis <royassisg@gmail.com>
2021-09-23 17:42:35 +0300

    cmt

--- Ex132.java
+++ Ex132.java
@@ -2,45 +2,51 @@ import java.util.Arrays;
 
 // Write a Java program to get the preorder traversal of its nodes' values of a given a binary tree
 
-
 public class Ex132 {
     public static Integer getRandomNumber(int min, int max) {
         return (int) ((Math.random() * (max - min)) + min);
     }
-    public static Integer[] getRandomArr(int size, int min, int max){
+
+    public static Integer[] getRandomArr(int size, int min, int max) {
         Integer[] arr = new Integer[size];
-        for (int i=0; i< size ; i++){
+        for (int i = 0; i < size; i++) {
             arr[i] = getRandomNumber(min, max);
         }
         return arr;
     }
-    public static Integer getSqueezedArrLength(Integer[] arr, Integer c){
-        Integer count =0;
+
+    public static Integer getSqueezedArrLength(Integer[] arr, Integer c) {
+        Integer count = 0;
         Integer tempCount = 0;
-        int i= 0;
-        int j= 0 ;
-        
-        while (i < arr.length){
-            for (j=i; j<= arr.length;j++) {
-                if (j==arr.length || arr[i] != arr[j]){
-                    i = j ;
+        int i = 0;
+        int j = 0;
+
+        while (i < arr.length) {
+            for (j = i; j <= arr.length; j++) {
+                // j==arr.length - this takes care of the last element and prevents an endless
+                // loop
+                if (j == arr.length || arr[i] != arr[j]) {
+                    i = j;
                     break;
-                }
-                else{
+                } else {
                     tempCount++;
                 }
             }
             count = count + (tempCount > c ? c : tempCount);
-            tempCount =0;
+            tempCount = 0;
         }
 
-        return count ;
+
+        return count;
     }
     
-
+    public static Integer getSqueezedArrLength2(Integer[] arr, Integer c) {
+        return 2;
+    }
 
     public static void main(String[] args) {
-        Integer[] arr = {1,2,3,3,3,4,4,5,6};
-        System.out.println(String.format("Array: %s have %d unique numbers", Arrays.toString(arr), getSqueezedArrLength(arr, 2)));
+        Integer[] arr = { 1, 2, 3, 3, 3, 4, 4, 5, 6 };
+        System.out.println(
+                String.format("Array: %s have %d unique numbers", Arrays.toString(arr), getSqueezedArrLength(arr, 2)));
     }
 }
