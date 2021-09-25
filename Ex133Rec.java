b88ec0f95f6140db2bee97a9d133d8cdeb9e6001
Roy Assis <royassisg@gmail.com>
2021-09-25 20:05:45 +0300

    cmt

--- Ex133Rec.java
+++ Ex133Rec.java
@@ -6,84 +6,74 @@ class Ex133Rec {
     public static void main(String[] args) {
         Matrix matrix = Matrix.generateRandomMatrix(3, 3, 1, 6);
         System.out.println(matrix);
-        // foo(matrix);
-        fooBar(matrix);
 
-    }
-
-    static void foo(Matrix matrix) { 
-        ArrayList<ArrayList<MatrixEle>> arrList = new ArrayList<ArrayList<MatrixEle>> ();
-        arrList.add(new ArrayList<MatrixEle>());
-        fooBase(matrix, new MatrixEle(), arrList, 0);
-
-        for (ArrayList<MatrixEle> c :arrList){
+        System.out.println(getMinPathSumOfMatrix(matrix));
+        for (ArrayList<MatrixEle> c : getAllMatrixPaths(matrix)) {
             System.out.println(c);
         }
+
     }
 
-    static Integer fooBase(Matrix matrix, MatrixEle currentLocation, ArrayList<ArrayList<MatrixEle>> arrList, int loc) {
+    static Integer getMinPathSumOfMatrix(Matrix matrix) {
+        return getMinPathSumOfMatrixBase(matrix, new MatrixEle());
+    }
+
+    static Integer getMinPathSumOfMatrixBase(Matrix matrix, MatrixEle currentLocation) {
         Integer resultsA = null;
         Integer resultsB = null;
-
-        arrList.get(loc).add(currentLocation);
 
         if (matrix.down(currentLocation) == null && matrix.right(currentLocation) == null) {
             return matrix.valueAtElement(currentLocation);
         }
-  
-        int a = loc;
-        int b = loc;
-        if (matrix.right(currentLocation) == null || matrix.down(currentLocation) == null ){
-            arrList.add(new ArrayList<>(arrList.get(loc)));
-            b++;
-        }
-        
+
         if (matrix.down(currentLocation) != null) {
-            resultsA = matrix.valueAtElement(currentLocation) + fooBase(matrix, new MatrixEle(currentLocation).addY(), arrList, a);
+            resultsA = matrix.valueAtElement(currentLocation)
+                    + getMinPathSumOfMatrixBase(matrix, new MatrixEle(currentLocation).addY());
         }
         if (matrix.right(currentLocation) != null) {
-            resultsB = matrix.valueAtElement(currentLocation) + fooBase(matrix, new MatrixEle(currentLocation).addX(), arrList, b);
+            resultsB = matrix.valueAtElement(currentLocation)
+                    + getMinPathSumOfMatrixBase(matrix, new MatrixEle(currentLocation).addX());
         }
-        
+
         if (resultsA == null) {
             return resultsB;
         } else if (resultsB == null) {
             return resultsA;
         }
-        
+
         return (resultsA > resultsB ? resultsB : resultsA);
     }
 
-    static void fooBar(Matrix matrix){
-        ArrayList<ArrayList<MatrixEle>> arrList = new ArrayList<ArrayList<MatrixEle>> ();
+    static ArrayList<ArrayList<MatrixEle>> getAllMatrixPaths(Matrix matrix) {
+        ArrayList<ArrayList<MatrixEle>> arrList = new ArrayList<ArrayList<MatrixEle>>();
         arrList.add(new ArrayList<MatrixEle>());
-        fooBarBase(matrix, new MatrixEle(), arrList, 0);
+        getAllMatrixPathsBase(matrix, new MatrixEle(), arrList, 0);
 
-        for (ArrayList<MatrixEle> c :arrList){
-            System.out.println(c);
-        }
+        return arrList;
     }
 
-    static void fooBarBase(Matrix matrix, MatrixEle currentLocation, ArrayList<ArrayList<MatrixEle>> arrList, int loc) {
+    static void getAllMatrixPathsBase(Matrix matrix, MatrixEle currentLocation, ArrayList<ArrayList<MatrixEle>> arrList,
+            int loc) {
 
         arrList.get(loc).add(currentLocation);
 
         if (matrix.down(currentLocation) == null && matrix.right(currentLocation) == null) {
             return;
         }
-  
+
         int a = loc;
         int b = loc;
-        if (matrix.right(currentLocation) != null && matrix.down(currentLocation) != null ){
+        if (matrix.right(currentLocation) != null && matrix.down(currentLocation) != null) {
             arrList.add(new ArrayList<>(arrList.get(loc)));
-            b = arrList.size()-1;
+            b = arrList.size() - 1;
         }
-        
+
         if (matrix.down(currentLocation) != null) {
-            fooBarBase(matrix, new MatrixEle(currentLocation).addY(), arrList, a);
+            getAllMatrixPathsBase(matrix, new MatrixEle(currentLocation).addY(), arrList, a);
         }
         if (matrix.right(currentLocation) != null) {
-            fooBarBase(matrix, new MatrixEle(currentLocation).addX(), arrList, b);
+            getAllMatrixPathsBase(matrix, new MatrixEle(currentLocation).addX(), arrList, b);
         }
+
     }
 }
