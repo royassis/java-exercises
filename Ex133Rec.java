a2017d3d0e37f7c63af20d8d65c5bcae30201f45
Roy Assis <royassisg@gmail.com>
2021-09-25 11:24:32 +0300

    cmt

--- Ex133Rec.java
+++ Ex133Rec.java
@@ -1,9 +1,4 @@
 import MatrixPackage.*;
-import java.util.Comparator;
-import java.util.Optional;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 class Ex133Rec {
     public static void main(String[] args) {
@@ -12,55 +7,33 @@ class Ex133Rec {
         System.out.println(foo(matrix));
 
     }
-    static Integer foo(Matrix matrix){
+
+    static Integer foo(Matrix matrix) {
         return fooBase(matrix, new MatrixEle());
     }
-    static Integer fooBase(Matrix matrix, MatrixEle currentLocation){
-        Integer resultsA=null;
-        Integer resultsB =null;
+
+    static Integer fooBase(Matrix matrix, MatrixEle currentLocation) {
+        Integer resultsA = null;
+        Integer resultsB = null;
         System.out.println(currentLocation);
-        if (matrix.down(currentLocation) == null && matrix.right(currentLocation) == null ){
+        if (matrix.down(currentLocation) == null && matrix.right(currentLocation) == null) {
             return matrix.valueAtElement(currentLocation);
 
-        }if(matrix.down(currentLocation) != null ){
-            resultsA = matrix.valueAtElement(currentLocation) + fooBase(matrix,new MatrixEle(currentLocation).addY());
+        }
+        if (matrix.down(currentLocation) != null) {
+            resultsA = matrix.valueAtElement(currentLocation) + fooBase(matrix, new MatrixEle(currentLocation).addY());
 
-        }if(matrix.right(currentLocation) != null){
-            resultsB = matrix.valueAtElement(currentLocation)+ fooBase(matrix, new MatrixEle(currentLocation).addX());
+        }
+        if (matrix.right(currentLocation) != null) {
+            resultsB = matrix.valueAtElement(currentLocation) + fooBase(matrix, new MatrixEle(currentLocation).addX());
         }
 
-        Stream<Integer> s= Stream.of(resultsA,resultsB);
-        Optional<Integer> o = s.collect(Collectors.maxBy(new IntegerComparator()));
-        // if (resultsA == null){
-        //     return resultsB;
-        // }
-        // else if (resultsB == null){
-        //     return resultsA;
-        // }
-        return (o.get());
-    }
-
-    static class IntegerComparator implements Comparator<Integer>{
-
-        @Override
-        public int compare(Integer arg0, Integer arg1) {
-            if (arg0 == null && arg1 == null){
-                return -2;
-            }
-            if (arg1 == null){
-                return 1;
-            }
-            if (arg0 == null){
-                return 1;
-            }
-            if (arg0 > arg1){
-                return 1;
-            }
-            else if (arg0 < arg1){
-                return -1;
-            }
-            return 0;
+        if (resultsA == null) {
+            return resultsB;
+        } else if (resultsB == null) {
+            return resultsA;
         }
+        return (resultsA > resultsB ? resultsB : resultsA);
     }
+
 }
-
