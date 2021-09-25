ebbee11c4043608f8fd6cc704179c739e03ce2f7
Roy Assis <royassisg@gmail.com>
2021-09-25 14:12:54 +0300

    cmt

--- Ex133Rec.java
+++ Ex133Rec.java
@@ -1,18 +1,21 @@
+import java.util.ArrayList;
+
 import MatrixPackage.*;
 
 class Ex133Rec {
     public static void main(String[] args) {
         Matrix matrix = Matrix.generateRandomMatrix(3, 3, 1, 6);
-        System.out.println(matrix + "\n");
-        System.out.println(foo(matrix));
+        foo(matrix);
 
     }
 
-    static Integer foo(Matrix matrix) {
-        return fooBase(matrix, new MatrixEle());
+    static Integer foo(Matrix matrix) { 
+        ArrayList<MatrixEle> arrList = new ArrayList<MatrixEle>();
+        arrList.add(new MatrixEle());
+        return fooBase(matrix, new MatrixEle(), arrList);
     }
 
-    static Integer fooBase(Matrix matrix, MatrixEle currentLocation) {
+    static Integer fooBase(Matrix matrix, MatrixEle currentLocation, ArrayList<MatrixEle> arrList) {
         Integer resultsA = null;
         Integer resultsB = null;
         System.out.println(currentLocation);
