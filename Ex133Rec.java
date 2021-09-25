36ec4b81b0a3867c7c9b0056d308bc1caf566bdf
Roy Assis <royassisg@gmail.com>
2021-09-25 19:59:15 +0300

    got it

--- Ex133Rec.java
+++ Ex133Rec.java
@@ -4,20 +4,21 @@ import MatrixPackage.*;
 
 class Ex133Rec {
     public static void main(String[] args) {
-        Matrix matrix = Matrix.generateRandomMatrix(2, 2, 1, 6);
+        Matrix matrix = Matrix.generateRandomMatrix(3, 3, 1, 6);
         System.out.println(matrix);
-        foo(matrix);
+        // foo(matrix);
+        fooBar(matrix);
 
     }
 
     static void foo(Matrix matrix) { 
-        Deque<MatrixEle> deque1 = new LinkedList<MatrixEle>();
-        Deque<MatrixEle> deque2 = new LinkedList<MatrixEle>();
-
         ArrayList<ArrayList<MatrixEle>> arrList = new ArrayList<ArrayList<MatrixEle>> ();
         arrList.add(new ArrayList<MatrixEle>());
         fooBase(matrix, new MatrixEle(), arrList, 0);
-        System.out.println(arrList);
+
+        for (ArrayList<MatrixEle> c :arrList){
+            System.out.println(c);
+        }
     }
 
     static Integer fooBase(Matrix matrix, MatrixEle currentLocation, ArrayList<ArrayList<MatrixEle>> arrList, int loc) {
@@ -31,12 +32,10 @@ class Ex133Rec {
         }
   
         int a = loc;
-        int b = loc + 1;
+        int b = loc;
         if (matrix.right(currentLocation) == null || matrix.down(currentLocation) == null ){
-            b = loc;
-        }
-        if (matrix.right(currentLocation) != null && matrix.down(currentLocation) != null ){
             arrList.add(new ArrayList<>(arrList.get(loc)));
+            b++;
         }
         
         if (matrix.down(currentLocation) != null) {
@@ -55,4 +54,36 @@ class Ex133Rec {
         return (resultsA > resultsB ? resultsB : resultsA);
     }
 
+    static void fooBar(Matrix matrix){
+        ArrayList<ArrayList<MatrixEle>> arrList = new ArrayList<ArrayList<MatrixEle>> ();
+        arrList.add(new ArrayList<MatrixEle>());
+        fooBarBase(matrix, new MatrixEle(), arrList, 0);
+
+        for (ArrayList<MatrixEle> c :arrList){
+            System.out.println(c);
+        }
+    }
+
+    static void fooBarBase(Matrix matrix, MatrixEle currentLocation, ArrayList<ArrayList<MatrixEle>> arrList, int loc) {
+
+        arrList.get(loc).add(currentLocation);
+
+        if (matrix.down(currentLocation) == null && matrix.right(currentLocation) == null) {
+            return;
+        }
+  
+        int a = loc;
+        int b = loc;
+        if (matrix.right(currentLocation) != null && matrix.down(currentLocation) != null ){
+            arrList.add(new ArrayList<>(arrList.get(loc)));
+            b = arrList.size()-1;
+        }
+        
+        if (matrix.down(currentLocation) != null) {
+            fooBarBase(matrix, new MatrixEle(currentLocation).addY(), arrList, a);
+        }
+        if (matrix.right(currentLocation) != null) {
+            fooBarBase(matrix, new MatrixEle(currentLocation).addX(), arrList, b);
+        }
+    }
 }
