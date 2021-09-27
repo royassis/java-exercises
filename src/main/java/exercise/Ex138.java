573a1799f520f971d464dfb938e6fcc436f5d0b2
Roy Assis <royassisg@gmail.com>
2021-09-27 18:07:10 +0300

    Ex138

---
+++ src/main/java/exercise/Ex138.java
@@ -0,0 +1,25 @@
+package exercise;
+
+import java.util.*;
+
+class Ex138 {
+    public static void main(String[] args) {
+        String[] strings = { "cat", "dog", "red", "is", "am" };
+        
+        ArrayList<String> longestStrings = new ArrayList<String>();
+        Integer longest = 0;
+
+        for (String str: strings){
+            if (str.length() > longest){
+                longest = str.length();
+                longestStrings = new ArrayList<String>();
+                longestStrings.add(str);
+            }else if(str.length() == longest){
+                longestStrings.add(str);
+            }
+        }
+
+        System.out.println(longestStrings);
+    }
+
+}
