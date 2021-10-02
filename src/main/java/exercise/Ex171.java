5a08abc07d4c1040f40cf584b4f585e38114f1dd
Roy Assis <royassisg@gmail.com>
2021-10-02 13:51:36 +0300

    Ex171

--- src/main/java/exercise/Ex171.java
+++ src/main/java/exercise/Ex171.java
@@ -3,8 +3,8 @@ package exercise;
 public class Ex171 {
 
     public static void main(String[] args) {
-        String a = "1";
-        String b = "a1";
+        String a = "1111";
+        String b = "101111";
 
         System.out.println(isStringContained(a, b));
 
@@ -15,17 +15,9 @@ public class Ex171 {
         int j = 0;
 
         while (j < b.length() && i < a.length()) {
-
-            if (a.charAt(i) == b.charAt(j)) {
-                i++;
-            } else {
-                i = 0;
-            }
+            i = a.charAt(i) == b.charAt(j) ? i + 1 : 0;
             j++;
-
         }
-
         return i == a.length();
-
     }
 }
