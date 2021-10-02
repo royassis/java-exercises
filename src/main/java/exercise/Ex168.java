a657c66ebcfd7228243998bafe3e63b8e7bd49e8
Roy Assis <royassisg@gmail.com>
2021-10-02 12:06:38 +0300

    Ex168

---
+++ src/main/java/exercise/Ex168.java
@@ -0,0 +1,18 @@
+package exercise;
+
+
+public class Ex168 {
+
+    public static void main(String[] args) {
+
+        int a= 2;
+        int b = 30;
+        int x =0;
+
+        for (int i=0; i< b; i++){
+            x =x +a;
+        }
+
+        System.out.println(x);
+    }
+}
