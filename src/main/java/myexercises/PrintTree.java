e04f4bf248bbcb90686770f5873374f770fcb4a9
Roy Assis <royassisg@gmail.com>
2021-10-03 08:44:50 +0300

    cmt

--- src/main/java/myexercises/PrintTree.java
+++ src/main/java/myexercises/PrintTree.java
@@ -9,7 +9,7 @@ public class PrintTree {
 
         int maxDepth = tree.maxDepth();
         int start =0 ;
-        int end = (int)(2*Math.pow(2, (double)maxDepth)-1);
+        int end = (int)(2*Math.pow(2, (double)maxDepth-1)-2);
         int currentDepth = 1;
         Node currentNode = tree.getRoot();
 
@@ -19,31 +19,38 @@ public class PrintTree {
     }
 
     public static void printTree(Node currentNode, int currentDepth , int maxDepth, int start, int end){
-        String str = "";
         int middle = (start+end)/2;
+        String c = "-";
 
-        if (currentDepth == maxDepth) {
+        if (currentDepth > maxDepth || start > end) {
             return;
         }
 
         //check if number
-        String numberOrSpace = currentNode != null ? Integer.toString(currentNode.getVal()) : " ";
+        String numberOrSpace = currentNode != null ? Integer.toString(currentNode.getVal()) : c;
 
         //before number
-        System.out.print(" ".repeat(maxDepth-currentDepth));
+        int nRepeats = (end-start)/2  ;
+
+        System.out.print(c.repeat(nRepeats));
 
         //number
         System.out.print(numberOrSpace);
 
         //after number
-        System.out.print(" ".repeat(maxDepth-currentDepth));
+        System.out.print(c.repeat(nRepeats));
 
-        if (currentNode.isLeft() || currentNode.isRight()){
+        // if (currentNode!=null && (currentNode.isLeft() || currentNode.isRight())){
+        //     System.out.println();
+        // }
+
+        if (end == 6){
             System.out.println();
         }
 
-        printTree(currentNode.getLeft(), currentDepth+1, maxDepth, start, middle);
-        printTree(currentNode.getRight(), currentDepth+1, maxDepth, middle+1, end);
+        printTree(currentNode != null ? currentNode.getLeft() : null, currentDepth+1, maxDepth, start, middle-1);
+        printTree(null, currentDepth+1, maxDepth, middle, middle);
+        printTree(currentNode != null ? currentNode.getRight() : null, currentDepth+1, maxDepth, middle+1, end);
         
     }
 
